46f65526f20ce7be84dd76058d66f5f14b74dd3a
Petar Mihaylov Petrov <Mihaylov93@users.noreply.github.com>
2019-12-11 20:59:50 +0100

    Merge to master
    * Add .travis.yml
    
    * Markdown cleanup
    
    * Reduce scope of variables

--- main.cpp
+++ main.cpp
@@ -8,28 +8,25 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("BatMon");
 
     QSharedPointer <BatteryWidget> w;
-
-    int mseconds = 0;
-    int mode = 0;
-    bool sConverted = false;
-    bool mConverted = false;
-
+    
     if (QCoreApplication::arguments().count() > 1){
-        mseconds = QCoreApplication::arguments().at(1).toInt(&sConverted) * 1000;
+        bool secondsConverted = false;
+        bool modeConverted = false;
+        int milliseconds = QCoreApplication::arguments().at(1).toInt(&secondsConverted) * 1000;
         if (QCoreApplication::arguments().count() == 3){
-             mode = QCoreApplication::arguments().at(2).toInt(&mConverted);
-             if(mConverted && sConverted){
-                 if (mseconds > 0){
-                     w = QSharedPointer <BatteryWidget>(new BatteryWidget (mseconds,mode));
+             int mode = QCoreApplication::arguments().at(2).toInt(&modeConverted);
+             if(modeConverted && secondsConverted){
+                 if (milliseconds > 0){
+                     w = QSharedPointer <BatteryWidget>(new BatteryWidget (milliseconds,mode));
                  }
                  else w = QSharedPointer <BatteryWidget>(new BatteryWidget (30000,mode));
 
               }
-             else w = QSharedPointer <BatteryWidget>(new BatteryWidget (mseconds,0));
+             else w = QSharedPointer <BatteryWidget>(new BatteryWidget (milliseconds,0));
         }else{
             //Test if the arg can be parsed into a int if not set default 30 seconds.
-            if(sConverted)
-                if (mseconds > 0) w = QSharedPointer <BatteryWidget>(new BatteryWidget (mseconds,0));
+            if(secondsConverted)
+                if (milliseconds > 0) w = QSharedPointer <BatteryWidget>(new BatteryWidget (milliseconds,0));
                 else  w = QSharedPointer <BatteryWidget>(new BatteryWidget (30000,0));
             else  w = QSharedPointer <BatteryWidget>(new BatteryWidget (30000,0));
         }
@@ -40,4 +37,3 @@ int main(int argc, char *argv[])
     w.data()->show();
     return QApplication::exec();
 }
-
